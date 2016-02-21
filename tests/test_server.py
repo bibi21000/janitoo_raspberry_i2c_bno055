ba21f1abecf7e57e6a06a0ff7475ad4341ec497a
bibi21000 <bibi21000@gmail.com>
2016-02-21 23:28:27 +0100

    Update tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -45,18 +45,6 @@ from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_S
 
 from janitoo_raspberry.server import PiServer
 
-##############################################################
-#Check that we are in sync with the official command classes
-#Must be implemented for non-regression
-from janitoo.classes import COMMAND_DESC
-
-COMMAND_DISCOVERY = 0x5000
-
-assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
-##############################################################
-
-#~ JNTTServer.skipRasperryTest()
-
 class TestPiSerser(JNTTServer, JNTTServerCommon):
     """Test the pi server
     """
@@ -70,4 +58,4 @@ class TestPiSerser(JNTTServer, JNTTServerCommon):
 
     def test_040_server_start_no_error_in_log(self):
         self.onlyRasperryTest()
-        JNTTServer.test_040_server_start_no_error_in_log(self)
+        JNTTServerCommon.test_040_server_start_no_error_in_log(self)

b1900401401a6b9d30311693fee2d8fa9a5c63de
bibi21000 <bibi21000@gmail.com>
2016-04-09 17:38:35 +0200

    Update tests for docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -43,3 +43,17 @@ from janitoo.utils import HADD_SEP, HADD
 
 from janitoo_raspberry.server import PiServer
 
+class TestPiSerser(JNTTDockerServer, JNTTDockerServerCommon):
+    """Test the server
+    """
+    loglevel = logging.DEBUG
+    path = '/tmp/janitoo_test'
+    broker_user = 'toto'
+    broker_password = 'toto'
+    server_class = PiServer
+    server_conf = "tests/data/janitoo_raspberry_i2c_bno055.conf"
+    hadds = [HADD%(144,0), HADD%(144,1)]
+
+    def test_040_server_start_no_error_in_log(self):
+        JNTTDockerServer.onlyDockerTest()
+        JNTTDockerServerCommon.minimal_040_server_start_reload_restart(self)
